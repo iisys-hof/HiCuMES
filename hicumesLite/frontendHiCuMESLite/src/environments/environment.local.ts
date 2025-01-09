cc3ad2c5ba2470f22d51a27525b695c4540bf3e2
Tobias Wirth <tobias@startpunkt.digital>
2025-01-09 14:11:25 +0100

    Added missing environments for build and fixed package versions

---
+++ hicumesLite/frontendHiCuMESLite/src/environments/environment.local.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://localhost:8080',
+  keycloakUrl: 'http://localhost:8181/',
+  keycloakRealm: 'hicumes',
+  keycloakClientId: 'Manufacturing_Frontend',
+  baseref: ''
+};
